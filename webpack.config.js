cfe4b48178d59c80faa825a0bf572052c4680754
gk90731 <gk90731@gmail.com>
2020-03-14 18:00:26 +0530

    React Webpack Integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            }, {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              },
+
+        ]
+    }
+}
